b2a6369ccb911c77f367c431654362d42cb6f619
kenneth <kennethbacaltos091@gmail.com>
2025-11-02 18:54:16 +0800

    Created route for updating landlord profile.

--- controller/landlordController.js
+++ controller/landlordController.js
@@ -1,6 +1,25 @@
 import { supabase } from "../server.js";
 import { decode } from "base64-arraybuffer";
 
+async function uploadProfile(id, image) {
+  const base64 = image.split(",")[1];
+  const mime = image.match(/:(.*?);/)[1];
+
+  const { data, error } = await supabase.storage
+      .from("profile")
+      .upload(id + "/" + "profile", decode(base64), {
+        contentType: mime,
+        cacheControl: "3600",
+        upsert: true,
+      });
+
+    if (error) {
+      return { status: false, error: error };
+    }
+
+    return { status: true, data: data }
+}
+
 export async function checkUserIfCompletedAccountSetup(req, res) {
   const userId = req.params.id;
 
@@ -14,7 +33,6 @@ export async function checkUserIfCompletedAccountSetup(req, res) {
       throw error;
     }
 
-    console.log(data);
     if (!data || data.length === 0) {
       return res.status(404).json({ message: "Record not found" });
     }
@@ -28,26 +46,18 @@ export async function checkUserIfCompletedAccountSetup(req, res) {
 
 export async function completeAccountSetup(req, res) {
   const { id, img, address, business_name, phone_number } = req.body;
-  const base64 = img.split(",")[1];
-  const mime = img.match(/:(.*?);/)[1];
 
   try {
-    const { data: uploadData, error: uploadError } = await supabase.storage
-      .from("profile")
-      .upload(id + "/" + "profile", decode(base64), {
-        contentType: mime,
-        cacheControl: "3600",
-        upsert: true,
-      });
+    const uploadImage = await uploadProfile(id, img);
 
-    if (uploadError) {
-      throw uploadError
+    if (!uploadImage.status) {
+      throw uploadImage.error
     }
 
     const { data, error } = await supabase
       .from("landlords")
       .update({
-        profile_pic: uploadData.fullPath,
+        profile_pic: uploadImage.data.fullPath,
         address,
         business_name,
         phone_number,
@@ -60,6 +70,108 @@ export async function completeAccountSetup(req, res) {
     }
 
     res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+}
+
+async function getProfileImage(userId) {
+  const { data, error } = await supabase.storage
+    .from("profile")
+    .list(userId + "/", {
+      limit: 1,
+    });
+
+  if (error) {
+    return { success: false, error: error };
+  }
+
+  return { success: true, data: data };
+}
+
+export async function getProfile(req, res) {
+  const userId = req.params.id;
+
+  try {
+    const { data, error } = await supabase
+      .from("landlords")
+      .select("*")
+      .eq("id", userId)
+      .single();
+
+    if (error) {
+      throw error;
+    }
+
+    // Get user profile
+    const profileImage = await getProfileImage(userId);
+
+    if (!profileImage.success) {
+      throw profileImage.error;
+    }
+
+    res.status(200).json({ userData: data, userProfile: profileImage.data });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
+}
+
+export async function EditLandlordProfile(req, res) {
+  const {
+    img,
+    id,
+    first_name,
+    last_name,
+    phone_number,
+    address,
+    business_name,
+  } = req.body;
+
+  try {
+    if (img === "") {
+      const { error } = await supabase
+        .from("landlords")
+        .update({
+          first_name: first_name,
+          last_name: last_name,
+          phone_number: phone_number,
+          address: address,
+          business_name: business_name
+        })
+        .eq("id", id);
+
+      if (error) {
+        throw error;
+      }
+
+      return res.status(200).json({ message: "Profile updated." });
+    }
+
+    const uploadImage = await uploadProfile(id, img);
+
+    if (!uploadImage.status) {
+      throw uploadImage.error
+    }
+
+    const { error } = await supabase
+      .from("landlords")
+      .update({
+        first_name: first_name,
+        last_name: last_name,
+        phone_number: phone_number,
+        address: address,
+        business_name: business_name,
+        profile_pic: uploadImage.data.fullPath
+      })
+      .eq("id", id);
+
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).json({ message: "Profile updated." });
   } catch (error) {
     console.error(error);
     res.status(400).json({ message: error.message });
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 
-import { checkUserIfCompletedAccountSetup, completeAccountSetup } from "../controller/landlordController.js";
+import {
+  checkUserIfCompletedAccountSetup,
+  completeAccountSetup,
+  getProfile,
+  EditLandlordProfile
+} from "../controller/landlordController.js";
 
 export const router = express.Router();
 
 router.get("/check-setup/:id", checkUserIfCompletedAccountSetup);
-router.post("/complete-setup", completeAccountSetup)
+router.post("/complete-setup", completeAccountSetup);
+router.get("/get-profile/:id", getProfile);
+router.post("/edit-landlord-profile", EditLandlordProfile)
